9ca4df0c13229b22df172f3fb5941932d4773b40
cal <cal@cals-iMac.local>
2024-10-14 08:48:23 -0700

    swift UI practice

---
+++ swiftUI-practice/swiftUI-practice/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  swiftUI-practice
+//
+//  Created by cal on 10/13/24.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
---
+++ swiftUI-practice/swiftUI-practice/swiftUI_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_practiceApp.swift
+//  swiftUI-practice
+//
+//  Created by cal on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
