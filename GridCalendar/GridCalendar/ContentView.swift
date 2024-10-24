fb481484c263537eb20d1a2481eb4c045183f68a
cal <cal@cals-iMac.local>
2024-10-23 18:59:17 -0700

    Add GridCalendar

---
+++ GridCalendar/GridCalendar/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  GridCalendar
+//
+//  Created by cal on 10/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    struct Day: Identifiable {
+        let id = UUID()
+        let value: Int
+    }
+    
+    struct Month {
+        let name: String
+        let numberOfDays: Int
+        var days: [Day]
+        
+        init(name: String, numberOfDays: Int) {
+            self.name = name
+            self.numberOfDays = numberOfDays
+            self.days = []
+            
+            for n in 1...numberOfDays {
+                self.days.append(Day(value: n))
+            }
+        }
+    }
+    
+    let year = [
+        Month(name: "January", numberOfDays: 31),
+        Month(name: "February", numberOfDays: 28),
+        Month(name: "March", numberOfDays: 31),
+        Month(name: "April", numberOfDays: 30),
+        Month(name: "May", numberOfDays: 31),
+        Month(name: "June", numberOfDays: 30),
+        Month(name: "July", numberOfDays: 31),
+        Month(name: "August", numberOfDays: 31),
+        Month(name: "September", numberOfDays: 30),
+        Month(name: "October", numberOfDays: 31),
+        Month(name: "November", numberOfDays: 30),
+        Month(name: "December", numberOfDays: 31)
+    ]
+    
+    let layout = [
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40))
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(year[0].days) { day in
+                    Capsule()
+                        .overlay(Text("\(day.value)").foregroundColor(.white))
+                        .foregroundColor(.blue)
+                        .frame(height: 40)
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridCalendar/GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by cal on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
