78f0adda0be111f1b6afaf65a7ceb04eedc5601f
cal <cal@cals-iMac.local>
2024-10-29 15:18:11 -0700

    Toolbar project

--- ToolBarProject/ToolBarProject/ContentView.swift
+++ ToolBarProject/ToolBarProject/ContentView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isPresenting: Bool = false
+
     var body: some View {
+        
         NavigationView {
             Text("Initial View")
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button(action: {
-                        
+                        isPresenting = true
                     }, label: {
                         Image(systemName: "gear")
                     })
+                }
+            }
+            .sheet(isPresented: $isPresenting) {
+                NavigationView {
+                    Text("Settings Page")
+                    .toolbar {
+                        ToolbarItem {
+                            Button("Save") {}
+                        }
+                        }
                 }
             }
         }
