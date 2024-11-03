807214f95ac070b48de2caa2cf71ae3657044894
cal <cal@cals-iMac.local>
2024-11-02 19:35:39 -0700

    Forms and Pickers

---
+++ ContactForm/ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by cal on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
