3efe0959405699b52f7baaf29d7facf7b6088a10
cnagyei <cnagyei15@gmail.com>
2024-11-10 17:21:45 +0000

    Force unwrapping

---
+++ Optionals/ForceUnwrapping.swift
@@ -0,0 +1,8 @@
+// Write your code below ✈️
+var flightDistance: Int = 1000
+var connectingFlightDistance: Int? = 2000
+
+if connectingFlightDistance != nil {
+  let totalFlightDistance = flightDistance + connectingFlightDistance!
+  print(totalFlightDistance)
+}
